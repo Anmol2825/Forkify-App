470d5d48773295cb05a2a935a89fc004fafd7357
Navneet-Raj99 <80098916+Anmol2825@users.noreply.github.com>
2022-10-12 16:28:15 +0530

    commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '2a5dcad6-a4bb-40e4-9909-1de4042cc21f';
 export const MODAL_CLOSE_SEC = 2.5;
